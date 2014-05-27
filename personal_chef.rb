60c3bd4f9319b698b8d0463e19a645aa8739d1d9
LandonWest <landonwest5@gmail.com>
2014-05-27 16:01:51 -0600

    add basic program code

--- personal_chef.rb
+++ personal_chef.rb
@@ -1,1 +1,29 @@
-new file
+class PersonalChef
+  attr_accessor :name
+
+  def initialize(name = "")
+    @name = name
+  end
+
+  def make_toast(doneness)
+    puts "Your toast will be #{doneness}"
+  end
+
+  def make_juice(juice_type)
+    puts "Your juice choice is #{juice_type}"
+  end
+
+  def make_eggs(quantity, style)
+    puts "Your #{quantity} eggs are cooked #{style}"
+  end
+
+end
+
+
+
+
+
+Gordon = PersonalChef.new
+Gordon.make_toast("black")
+Gordon.make_juice("apple")
+Gordon.make_eggs(2, "overeasy")
